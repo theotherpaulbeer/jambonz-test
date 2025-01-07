0e3a846e1be5dc196230f20305ef41d9abe38a30
Paul Beer <paul_r_beer@keybank.com>
2025-01-07 15:40:33 -0500

    add jambonz webhook

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps : [{
+    name: 'paultest',
+    script: 'app.js',
+    instance_var: 'INSTANCE_ID',
+    exec_mode: 'fork',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      LOGLEVEL: 'info',
+      HTTP_PORT: 3000,
+      JAMBONZ_ACCOUNT_SID: '',
+      JAMBONZ_API_KEY: '',
+      JAMBONZ_REST_API_BASE_URL: '',
+      WEBHOOK_SECRET: '',
+      HTTP_PASSWORD: '',
+      HTTP_USERNAME: '',
+    }
+  }]
+};
