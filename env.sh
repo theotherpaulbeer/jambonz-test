2c7468edbab15a20e979e37dff0293eebf119b77
Paul Beer <paul_r_beer@keybank.com>
2025-01-16 14:41:23 -0500

    play dummy message

---
+++ env.sh
@@ -0,0 +1,3 @@
+export JAMBONZ_ACCOUNT_SID=e11b8c9a-e4c4-4f6f-8e28-0b355748d68e
+export JAMBONZ_API_KEY=fbd68cd3-83cd-45ec-9152-fc4650bb19e4
+export JAMBONZ_REST_API_BASE_URL=https://jambonz-test-production.up.railway.app/
